77ec5959f094e2cfe4b0430a97395e44bf872a56
Imme <andeser@protonmail.com>
2021-09-10 16:16:47 +0200

    Added xmlhttprequest

--- routes/api.js
+++ routes/api.js
@@ -6,22 +6,17 @@ module.exports = function (app) {
     .get(function (req, res){
       const stock = req.query.stock;
       const likes = req.query.likes;
-      const url = 'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/' + stock + '/quote';
-      let newXHR = new XMLHttpRequest();
-      newXHR.open("GET", url);
-      newXHR.onreadystatechange = () => {
-        if (this.status === 200 && this.readyState === 4){
-          data = JSON.parse(this.response);
-          res.send({
-            stockData: {
-              stock: stock,
-              price: data.latestPrice,
-              likes: 1
-            }
-          });
-        }
-      }
-
+      fetch('https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/' + stock + '/quote')
+      .then(response => response.json())
+      .then(data => {
+        res.send({
+          stockData: {
+            stock: stock,
+            price: data.latestPrice,
+            likes: 1
+          }
+        });
+      });
       
     });
     
